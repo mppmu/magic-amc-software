48bea1678c7acd098e070714f6a58a76909272fa
Markus Fras <fras@mpp.mpg.de>
2025-06-26 13:52:06 +0200

    Added recipe to build the openSUSE 10.2 Apptainer container and scripts to launch the container.

---
+++ depend/apptainer/opensuse/10.2/apptainer-magic-amc-suse-sandbox.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# File: apptainer-magic-amc-suse-sandbox.sh
+# Auth: M. Fras, Electronics Division, MPI for Physics, Munich
+# Mod.: M. Fras, Electronics Division, MPI for Physics, Munich
+# Date: 22 May 2025
+# Rev.: 26 Jun 2025
+#
+# Shell script for launching a shell within the MAGIC AMC openSUSE 10.2 container.
+#
+
+export PROJECT_INFO_MPI="<MAGIC AMC - openSUSE 10.2 (sandbox)> "
+apptainer exec $@ -B /opt /opt/Apptainer/Images/magic-amc-suse bash
+
